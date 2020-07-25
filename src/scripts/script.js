3c3d45994023aac6c93cb9425a601eeb2254c2ac
oelfernandez <oelfernandeztorres@gmail.com>
2020-07-25 18:08:31 -0430

    In progress gym

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,14 @@
 import $ from 'jquery';
-
-$(document).ready(function(){
-    $('.button-burger').click(function(){
+//Activar Menú Desplegable
+$(document).ready(function () {
+    $('.button-burger').click(function () {
         $('.menu nav').toggleClass('active');
     });
-
-    $('.question-reply li').click(function(e) {
+//Activar Menú Acordeon
+    $('.question-reply li').click(function (e) {
         e.preventDefault();
 
-        if ($(this).hasClass('activate')){
+        if ($(this).hasClass('activate')) {
             $(this).removeClass('activate');
             $(this).children('ul').slideUp();
         } else {
@@ -18,5 +18,72 @@ $(document).ready(function(){
             $(this).children('ul').slideDown();
 
         }
+    });
+//Activar Modal Lightbox
+    $('.gallery-img').click(function (event) { // hacer click en la imagen
+        event.preventDefault(); //prevenir los efectos del a
+
+        if (0 < $(this).next().length){
+            $('.next').show();
+        } else {
+            $('.next').hide();
+        }
+
+        if (0 < $(this).prev().length){
+            $('.prev').show();
+        } else {
+            $('.prev').hide();
+        }
+
+        $('#modal').find('img').attr('src', $(this).find('img').attr('src'));
+        $('#modal').show();
+    });
+
+    $('.close').click(function (event) { //hacer click en boton cerrar
+        event.preventDefault();
+
+        $('#modal').hide();
+    });
+
+    $('.next, #modal img').click(function (event) { //hacer click en siguiente
+        event.preventDefault();
+
+        $('#modal').find('img').attr('src', $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().find('img').attr('src'));
+
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().length){
+            $('.next').show();
+        } else {
+            $('.next').hide();
+        }
+
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().length){
+            $('.prev').show();
+        } else {
+            $('.prev').hide();
+        }
+    });
+
+    $('.prev').click(function (event) { //hacer click atras
+        event.preventDefault();
+
+        $('#modal').find('img').attr('src', $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().find('img').attr('src'));
+
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().length){
+            $('.next').show();
+        } else {
+            $('.next').hide();
+        }
+
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().length){
+            $('.prev').show();
+        } else {
+            $('.prev').hide();
+        }
+    });
+
+    $('#modal').click(function (event) {
+        $('#modal').hide();
+    }).children().click(function (event) {
+        return false;
     });
 });
