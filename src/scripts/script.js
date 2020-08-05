9520abbc68ad99b3b36621da215da7f4034c8d91
oelfernandez <oelfernandeztorres@gmail.com>
2020-08-04 19:33:08 -0430

    in progress gym IIIII

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
     $('.button-burger').click(function () {
         $('.menu nav').toggleClass('active');
     });
+
+    //Slider
+   /* $('.listado-testimoniales').bxSlider();*/
+
 //Activar Menú Acordeon
     $('.question-reply li').click(function (e) {
         e.preventDefault();
@@ -23,13 +27,13 @@ $(document).ready(function () {
     $('.gallery-img').click(function (event) { // hacer click en la imagen
         event.preventDefault(); //prevenir los efectos del a
 
-        if (0 < $(this).next().length){
+        if (0 < $(this).next().length) {
             $('.next').show();
         } else {
             $('.next').hide();
         }
 
-        if (0 < $(this).prev().length){
+        if (0 < $(this).prev().length) {
             $('.prev').show();
         } else {
             $('.prev').hide();
@@ -50,13 +54,13 @@ $(document).ready(function () {
 
         $('#modal').find('img').attr('src', $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().find('img').attr('src'));
 
-        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().length){
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().length) {
             $('.next').show();
         } else {
             $('.next').hide();
         }
 
-        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().length){
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().length) {
             $('.prev').show();
         } else {
             $('.prev').hide();
@@ -68,13 +72,13 @@ $(document).ready(function () {
 
         $('#modal').find('img').attr('src', $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().find('img').attr('src'));
 
-        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().length){
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').next().length) {
             $('.next').show();
         } else {
             $('.next').hide();
         }
 
-        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().length){
+        if (0 < $('.gallery-img img[src="' + $('#modal').find('img').attr('src') + '"]').parents('.gallery-img').prev().length) {
             $('.prev').show();
         } else {
             $('.prev').hide();
@@ -90,26 +94,45 @@ $(document).ready(function () {
 // Mapa de LeaFlet
 
 
+    /*  var map = L.map('map').setView([51.505, -0.09], 13);
 
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+      L.marker([51.5, -0.09]).addTo(map)
+          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+          .openPopup();*/
+//slider
+    $('.dot-button-button').click(function () {
+        $('.slider-item').removeClass('active');
+        $('.slider-item:nth-child(' + $(this).data('item') + ')').addClass('active');
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        $('.dot-button-button').removeClass('active');
+        $(this).addClass('active');
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    });
 
-
-
-
+    setTimeout(slider_play, 8000);
 
 });
 
+function slider_play() {
+    var quantity = $('.slider-item').length;
 
+    if (quantity == $('.slider-item.active').data('item')) {
+        $('.slider-item').removeClass('active');
+        $('.slider-item:first-child').addClass('active');
+        //button
+        $('.dot-button-button').removeClass('active');
+        $('.dot-button-button:first-child').addClass('active');
+    } else {
+        $('.slider-item.active').removeClass('active').next().addClass('active');
+        $('.dot-button-button.active').removeClass('active').next().addClass('active');
+    }
 
+    setTimeout(slider_play, 8000);
+}
 
 
 $(document).ready(function () {
